35ddbe813eba9194f0bd2f1aa369f31a104b0759
Matt Seidholz <mseidholz@gmail.com>
2020-05-12 10:43:33 -0400

    got '/download' path firing, but not with the file we want yet

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use(cors())
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const formidable = require('formidable');
+const fs = require('fs')
 
 const h0 = require('./controllers/h0')
 const h1 = require('./controllers/h1')
@@ -12,6 +13,8 @@ const h2 = require('./controllers/h2')
 const h3 = require('./controllers/h3')
 const h4 = require('./controllers/h4')
 const h5 = require('./controllers/h5')
+
+const fileName = [];
 
 
 app.use( (req, res, next) => {
@@ -37,14 +40,24 @@ app.post('/upload', (req,res) => {
     if (err) {
         console.error('Error', err)
         throw err
-    }   
+    }
     console.log('Fields', fields);
     console.log('Files', files);
+
     for (const file of Object.entries(files)) {
-        console.log(file);
-    }
+        console.log("The file is", file[1].name);
+        res.status(200).send(file[1].name + ' ' + "is fuckin uploaded you dumb bitch!")
+        fileName.push(file[1].name);
+        return console.log(fileName);
+        }
     })
-    res.send("it's fuckin done you dumb bitch")
+});
+
+// Okay, got dl working!
+// Just need to connect the upload above with the DL below!
+
+app.get('/download', (req, res) => {
+    res.download('./uploads/Why NRC Bullets[ms].docx')
 })
 
 module.exports = app;
