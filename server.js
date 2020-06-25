0cef537bbe3f46af96eb6c710d213f907966f1d0
Matt Seidholz <mseidholz@gmail.com>
2020-06-25 09:54:36 -0400

    specifiying env variables

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app")
 
-app.listen(3001, () => {
-    console.log("Live and listening on port 3001")
+
+const PORT = PROCESS.ENV.PORT || 3001 
+app.listen(PORT, () => {
+    console.log(`Live and listening on port ${PORT}`)
 })
